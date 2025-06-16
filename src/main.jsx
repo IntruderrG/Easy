7d5c164a6d29a7c2a077db0fb98f702cab5d9980
Gauravendra <gdevtripathi@gmail.com>
2025-06-16 15:20:31 +0530

    Commit-3 Updated footer.jsx to Footer.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import Navbar from "./Components/Navbar/Navbar.jsx";
-import Footer from "./Components/Navbar/Footer/footer.jsx";
+import Footer from "./Components/Navbar/Footer/Footer.jsx";
 import LiveLocationMap from "./WorkingComponents/LiveLocationMap.jsx";
 
 createRoot(document.getElementById("root")).render(
